693e920edada0c9db4be439f4bbbd3c0e198d134
Ariel Fernando <arielfernandoalves@hotmail.com>
2025-01-18 10:38:06 -0300

    Versão 2

--- ATV02MicrocontroladoresGPIO.c
+++ ATV02MicrocontroladoresGPIO.c
@@ -1,44 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 
-// Definições dos GPIOs utilizados
-#define LED_VERDE 11
-#define LED_AZUL 12
-#define LED_VERMELHO 13
+// Definição dos pinos
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
 #define BUZZER 21
 
-// Prototipação das funções
-void ligar_led_verde();
-void ligar_led_azul();
-void ligar_led_vermelho();
-void ligar_todos_leds();
-void desligar_todos_leds();
-void acionar_buzzer();
-void entrar_modo_gravacao();
+// Função para desligar todos os LEDs
+void turn_off_all() {
+    gpio_put(LED_GREEN, 0);
+    gpio_put(LED_BLUE, 0);
+    gpio_put(LED_RED, 0);
+}
 
-int main() {
-    // Inicialização do UART e dos GPIOs
-    stdio_init_all();
-    gpio_init(LED_VERDE);
-    gpio_set_dir(LED_VERDE, GPIO_OUT);
-    gpio_init(LED_AZUL);
-    gpio_set_dir(LED_AZUL, GPIO_OUT);
-    gpio_init(LED_VERMELHO);
-    gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+// Função para ligar um LED específico
+void light_led(uint led_pin) {
+    turn_off_all();
+    gpio_put(led_pin, 1);
+}
+
+// Função para acionar o buzzer
+void activate_buzzer() {
+    gpio_put(BUZZER, 1);
+    sleep_ms(2000); // Aciona por 2 segundos
+    gpio_put(BUZZER, 0);
+}
+
+// Configuração inicial dos pinos
+void setup() {
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+
     gpio_init(BUZZER);
     gpio_set_dir(BUZZER, GPIO_OUT);
+}
 
+int main() {
+    stdio_init_all();
+    setup();
 
-    while (1) {
-        // Loop infinito para manter o programa rodando (se necessário)
-    ligar_led_verde();
-    ligar_led_azul();
-    ligar_led_vermelho();
-    ligar_todos_leds();
-    desligar_todos_leds();
-    acionar_buzzer();
-    entrar_modo_gravacao();
+    while (true) {
+        char command;
+        printf("Enter command: ");
+        command = getchar(); // Recebe comando via terminal
 
+        switch (command) {
+            case '1': // Ligar LED verde
+                light_led(LED_GREEN);
+                printf("LED verde ligado.\n");
+                break;
+            case '2': // Ligar LED azul
+                light_led(LED_BLUE);
+                printf("LED azul ligado.\n");
+                break;
+            case '3': // Ligar LED vermelho
+            case '4': // Ligar os três LEDs
+            case '5': // Desligar todos os LEDs
+            case '6': // Acionar o buzzer
+            default:
+                printf("Comando inválido. Tente novamente.\n");
+                break;
+        }
     }
 
     return 0;
